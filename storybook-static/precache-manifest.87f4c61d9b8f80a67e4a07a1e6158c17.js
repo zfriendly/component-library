2558663d3cb6f5c6de33120dc68a8dfb65dc2306
Zack Friendly <friendly.wisconsin@gmail.com>
2020-02-24 15:46:25 -0500

    Added storybook static

---
+++ storybook-static/precache-manifest.87f4c61d9b8f80a67e4a07a1e6158c17.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9b72ffc5f74e935f6376e5e2ef807a7",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.8003b979e9c8c4949a78.bundle.js"
+  },
+  {
+    "url": "runtime~main.8003b979e9c8c4949a78.bundle.js"
+  },
+  {
+    "revision": "fa9b24e4d31c546b6061",
+    "url": "static/css/main.189c05cd.chunk.css"
+  },
+  {
+    "revision": "1a80d912375498d3539cd515d8921c52",
+    "url": "static/media/arrow.1a80d912.svg"
+  },
+  {
+    "revision": "f061aae4ac0a7b27111d0fc93ee9bfc6",
+    "url": "static/media/cart.f061aae4.svg"
+  },
+  {
+    "revision": "0ae49bb896d2b79458854626af2d38db",
+    "url": "static/media/check.0ae49bb8.svg"
+  },
+  {
+    "revision": "cce5b434e5d3292b940b8f39bcb56216",
+    "url": "static/media/heart.cce5b434.svg"
+  },
+  {
+    "url": "vendors~main.8003b979e9c8c4949a78.bundle.js"
+  },
+  {
+    "url": "vendors~main.8003b979e9c8c4949a78.bundle.js.LICENSE.txt"
+  }
+]);
